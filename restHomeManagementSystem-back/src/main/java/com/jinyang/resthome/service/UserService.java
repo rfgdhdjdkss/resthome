45a685d3b759145bf05aa7f181db1503d7213f89
rfgdhdjdkss <107388538+rfgdhdjdkss@users.noreply.github.com>
2025-03-12 18:04:36 +0800

    第十一天

--- restHomeManagementSystem-back/src/main/java/com/jinyang/resthome/service/UserService.java
+++ restHomeManagementSystem-back/src/main/java/com/jinyang/resthome/service/UserService.java
@@ -15,51 +15,22 @@ import java.util.List;
  * @createDate 2024-10-02 20:35:36
  */
 public interface UserService extends IService<User> {
-
-
-    //登录业务
-    public Result login(User user);
-
-    //注册业务
-    public Result register(User user);
-
-    //查询所有用户信息业务
-    public Page<User> findAllUser(Page<User> page);
-
-    //根据id删除用户业务
-    public Result deleteUserByUid(Long uid);
-
-    //搜索框模糊查询业务
-    public Page<User> selectBySearchValue(Page<User> page, String searchValue);
-
-    //根据username更新数据业务
-    public Result updateUserByUsername(User user);
-
-    //根据uid获取余额业务
-    public List selectBalance(Long uid);
-
-    //根据uid充值业务
-    public int rechargeBalance(long uid, Double recharge);
-
-    //根据uid扣除余额业务
-    public int deductBalance(long uid, Double money);
-
-    //根据uid查找用户信息业务
-    User selectUserById(Long uid);
-
-    Result updatePassword(Long uid, String originalPassword, String newPassword1, String newPassword2);
-
-    void updateHeadImgUrlByUid(String newFileName, Long uid);
-
-    Result loginByUsername(loginByUsernameRequest user);
-
-    Result loginByPhone(loginByUsernameRequest user);
-
-    Result registerWithApp(User user);
-
-    Result updatePhone(Long uid, String phone);
-
-    Result updateNickname(Long uid, String nickname);
-
-    Result updateSex(Long uid, String sex);
+    Result login(User user);   //登录业务
+    Result register(User user);  //注册业务
+    Page<User> findAllUser(Page<User> page);//查询所有用户信息业务
+    Result deleteUserByUid(Long uid);   //根据id删除用户业务
+    Page<User> selectBySearchValue(Page<User> page, String searchValue);    //搜索框模糊查询业务
+    Result updateUserByUsername(User user); //根据username更新数据业务
+    List selectBalance(Long uid);    //根据uid获取余额业务
+    int rechargeBalance(long uid, Double recharge); //根据uid充值业务
+    int deductBalance(long uid, Double money);   //根据uid扣除余额业务
+    User selectUserById(Long uid);  //根据uid查找用户信息业务
+    Result updatePassword(Long uid, String originalPassword, String newPassword1, String newPassword2); //修改密码业务
+    void updateHeadImgUrlByUid(String newFileName, Long uid);   //根据id修改用户头像业务
+    Result loginByUsername(loginByUsernameRequest user);    //通过用户名登录业务（app端）
+    Result loginByPhone(loginByUsernameRequest user);   //通过手机号登录业务（app端）
+    Result registerWithApp(User user);  //注册业务（app端）
+    Result updatePhone(Long uid, String phone); //修改用户绑定的手机号业务
+    Result updateNickname(Long uid, String nickname);   //修改用户昵称业务
+    Result updateSex(Long uid, String sex); //修改用户性别业务
 }
